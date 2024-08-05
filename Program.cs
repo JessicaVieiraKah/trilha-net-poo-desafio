a36e4530061e63f19bd49f3d0d84f4aa72c9f0d1
Jéssica <139244540+JessicaVieiraKah@users.noreply.github.com>
2024-08-05 16:49:48 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,22 @@
-﻿using DesafioPOO.Models;
+using System;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Testando a classe Nokia
+        Smartphone nokia = new Nokia("123456789", "Nokia Modelo X", "111111111111111", 64);
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("WhatsApp");
+
+        Console.WriteLine();
+
+        // Testando a classe iPhone
+        Smartphone iphone = new Iphone("987654321", "iPhone Modelo Y", "222222222222222", 128);
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Instagram");
+    }
+}
